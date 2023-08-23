b743d2614e77ca7f60520992f0d39e5436f24acd
Rolando <rolando.arguello@gmail.com>
2023-08-23 16:30:44 -0600

    gdg

--- src/app/Contabilidad/ejercicio-fiscal/ejercicio-fiscal.component.ts
+++ src/app/Contabilidad/ejercicio-fiscal/ejercicio-fiscal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validacion } from 'src/SHARED/class/validacion';
 
 @Component({
   selector: 'app-ejercicio-fiscal',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./ejercicio-fiscal.component.scss']
 })
 export class EjercicioFiscalComponent {
+  public val = new Validacion();
   
+  constructor()
+  {
+    this.val.add("txtCuenta", "1", "LEN>", "0", "Cuenta", "Ingrese un número de cuenta.");
+    this.val.add("cmbNivel", "1", "LEN>", "0", "Nivel", "Seleccione un nivel.");
+    this.val.add("txtDescripcion", "1", "LEN>", "0", "Descripción", "Ingrese la descripción de la cuenta.");
+    this.val.add("txtCuentaPadre", "1", "LEN>=", "0", "Cuenta Padre", "");
+    this.val.add("txtDescripcionPadre", "1", "LEN>=", "0", "Descripcion Cuenta Padre", "");
+    this.val.add("cmbGrupo", "1", "LEN>", "0", "Group", "Seleccione un grupo.");
+    this.val.add("cmbClase", "1", "LEN>", "0", "Clase", "Seleccione una clase.");
+    this.val.add("chkBloqueada", "1", "LEN>", "0", "Bloqueada","");
+  }
+  
+
+ngOnInit() : void{
+
+  let lstcheckbox: any = document.querySelectorAll("input[type='checkbox']");
+    lstcheckbox.forEach((f: any) => {
+      f.bootstrapToggle();
+    });
+ 
+}
+
   }
