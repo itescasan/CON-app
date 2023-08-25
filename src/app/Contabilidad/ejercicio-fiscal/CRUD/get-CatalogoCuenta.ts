5a2780d9cd08a7dfd41fd2e93a113d0fb73bd5da
Rolando <rolando.arguello@gmail.com>
2023-08-25 08:56:27 -0600

    c

---
+++ src/app/Contabilidad/ejercicio-fiscal/CRUD/get-CatalogoCuenta.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpXhrBackend } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { Conexion } from "src/SHARED/class/Cadena_Conexion";
+
+@Injectable({
+    providedIn: 'root',
+  })
+export class getCuentaContable{
+    
+    private _Cnx = new Conexion();
+    private http: HttpClient;
+
+    constructor(){
+
+        this.http = new HttpClient(new HttpXhrBackend({ 
+            build: () => new XMLHttpRequest() 
+        }));
+
+    }
+
+ 
+   public Datos() : Observable<string>{
+      return this.http.get<any>(this._Cnx.Url() + "Contabilidad/CatalogoCuenta/Datos");
+   }
+
+   public Get() : Observable<string>{
+    return this.http.get<any>(this._Cnx.Url() + "Contabilidad/CatalogoCuenta/Get");
+ }
+   
+
+}
