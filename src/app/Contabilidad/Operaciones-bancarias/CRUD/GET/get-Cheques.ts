ab76a06d1e312e2ff94211858f9151349e3b65b4
Rolando <rolando.arguello@gmail.com>
2023-10-17 13:32:37 -0600

    tet

---
+++ src/app/Contabilidad/Operaciones-bancarias/CRUD/GET/get-Cheques.ts
@@ -0,0 +1,34 @@
+
+import { HttpClient, HttpXhrBackend } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { Conexion } from "src/app/SHARED/class/Cadena_Conexion";
+
+
+@Injectable({
+    providedIn: 'root',
+  })
+export class getCheques{
+    
+    private _Cnx = new Conexion();
+    private http: HttpClient;
+
+    constructor(){
+
+        this.http = new HttpClient(new HttpXhrBackend({ 
+            build: () => new XMLHttpRequest() 
+        }));
+
+    }
+
+ 
+   public Datos() : Observable<string>{
+      return this.http.get<any>(this._Cnx.Url() + "Contabilidad/Cheques/Datos");
+   }
+
+   public Get() : Observable<string>{
+    return this.http.get<any>(this._Cnx.Url() + "Contabilidad/Cheques/Get");
+ }
+   
+
+}
