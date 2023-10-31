4ded0341a7d5fa9fccbff65650d620a266455506
JairMartinez86 <jmartinezg86@gmail.com>
2023-10-31 16:28:16 -0600

    Asiento

--- src/app/Contabilidad/asiento-contable/nuevo-asiento-contable/asiento-contable/asiento-contable.component.ts
+++ src/app/Contabilidad/asiento-contable/nuevo-asiento-contable/asiento-contable/asiento-contable.component.ts
@@ -871,7 +871,19 @@ export class AsientoContableComponent {
   
 
 
+  ngDoCheck(){
 
+    this.val.addNumberFocusIn();
+
+    let i : number = 0;
+    this.lstDetalle.data.forEach(f => {
+      this.valTabla.addFocusOut("txtDebito" + i, 2);
+      this.valTabla.addFocusOut("txtCredito" + i, 2);
+      i++;
+    });
+
+      
+  }
 
 }
 
