da55a78c572b643a050fb7f3a8ac0e4b2ad041bd
JairMartinez86 <jmartinezg86@gmail.com>
2023-11-27 17:31:51 -0600

    balanza

--- src/app/Reporte/balanza/balanza.component.ts
+++ src/app/Reporte/balanza/balanza.component.ts
@@ -17,158 +17,205 @@ import * as printJS from 'print-js';
   styleUrls: ['./balanza.component.scss']
 })
 export class BalanzaComponent {
- 
- 
+
+
   public val = new Validacion();
-  
-  constructor(private cFunciones: Funciones, private GET : getReporteContable
-    ) {
-  
-      this.val.add("txtFecha1", "1", "LEN>", "0", "Fecha Inicio", "Seleccione una fecha de inicio.");
-      this.val.add("txtFecha2", "1", "LEN>", "0", "Fecha Final", "Seleccione una fecha final.");
-      this.val.add("cmbNivel", "1", "LEN>", "0", "Nivel", "Seleccione un nivel.");
-      this.val.add("cmbMoneda", "1", "LEN>", "0", "Moneda", "Selecione una moneda");
-      
-  
-      this.val.Get("txtFecha1").setValue(this.cFunciones.DateFormat((new Date(this.cFunciones.FechaServer.getFullYear(), this.cFunciones.FechaServer.getMonth(), 1)), "yyyy-MM-dd"));
-      this.val.Get("txtFecha2").setValue(this.cFunciones.DateFormat(this.cFunciones.FechaServer, "yyyy-MM-dd"));
-      this.val.Get("cmbNivel").setValue(1);
-      this.val.Get("cmbMoneda").setValue(true);
-    }
-  
 
-    V_Imprimir() : void{
+  constructor(private cFunciones: Funciones, private GET: getReporteContable
+  ) {
+
+    this.val.add("txtFecha1", "1", "LEN>", "0", "Fecha Inicio", "Seleccione una fecha de inicio.");
+    this.val.add("txtFecha2", "1", "LEN>", "0", "Fecha Final", "Seleccione una fecha final.");
+    this.val.add("cmbNivel", "1", "LEN>", "0", "Nivel", "Seleccione un nivel.");
+    this.val.add("cmbMoneda", "1", "LEN>", "0", "Moneda", "Selecione una moneda");
 
 
-      document.getElementById("btnReporte-Balanza")?.setAttribute("disabled", "disabled");
+    this.val.Get("txtFecha1").setValue(this.cFunciones.DateFormat((new Date(this.cFunciones.FechaServer.getFullYear(), this.cFunciones.FechaServer.getMonth(), 1)), "yyyy-MM-dd"));
+    this.val.Get("txtFecha2").setValue(this.cFunciones.DateFormat(this.cFunciones.FechaServer, "yyyy-MM-dd"));
+    this.val.Get("cmbNivel").setValue(1);
+    this.val.Get("cmbMoneda").setValue(true);
+  }
+
+
+  V_Imprimir(): void {
+
+
+    document.getElementById("btnReporte-Balanza")?.setAttribute("disabled", "disabled");
 
 
 
-      let dialogRef : any = this.cFunciones.DIALOG.getDialogById("wait") ;
-  
-  
-        if(dialogRef == undefined)
+    let dialogRef: any = this.cFunciones.DIALOG.getDialogById("wait");
+
+
+    if (dialogRef == undefined) {
+      dialogRef = this.cFunciones.DIALOG.open(
+        WaitComponent,
         {
-          dialogRef = this.cFunciones.DIALOG.open(
-            WaitComponent,
-            {
-              panelClass: "escasan-dialog-full-blur",
-              data: "",
-              id : "wait"
-            }
-          );
-    
-        }
-  
-  
-  
-      this.GET.GetBalanzaComprobacion(this.val.Get("txtFecha1").value, this.val.Get("txtFecha2").value, this.val.Get("cmbNivel").value, this.val.Get("cmbMoneda").value).subscribe(
-        {
-          next: (data) => {
-  
-  
-            dialogRef.close();
-            let _json: any = data;
-  
-            if (_json["esError"] == 1) {
-              if (this.cFunciones.DIALOG.getDialogById("error-servidor-msj") == undefined) {
-                this.cFunciones.DIALOG.open(DialogErrorComponent, {
-                  id: "error-servidor-msj",
-                  data: _json["msj"].Mensaje,
-                });
-              }
-            } else {
-  
-              let datos: iDatos = _json["d"];
-              this.printPDFS(datos.d);
-  
-         
-  
-            }
-  
-          },
-          error: (err) => {
-  
-            
-            dialogRef.close();
-            document.getElementById("btnReporte-Balanza")?.removeAttribute("disabled");
-            if (this.cFunciones.DIALOG.getDialogById("error-servidor") == undefined) {
-              this.cFunciones.DIALOG.open(DialogErrorComponent, {
-                id: "error-servidor",
-                data: "<b class='error'>" + err.message + "</b>",
-              });
-            }
-  
-          },
-          complete: () => {
-            document.getElementById("btnReporte-Balanza")?.removeAttribute("disabled");
-  
-  
-          }
+          panelClass: "escasan-dialog-full-blur",
+          data: "",
+          id: "wait"
         }
       );
-  
 
     }
 
 
-    async printPDFS(datos : any) {
 
-   
-    
-      let byteArray = new Uint8Array(atob(datos).split('').map(char => char.charCodeAt(0)));
-  
-      var file = new Blob([byteArray], { type: 'application/pdf' });
-  
-      let url = URL.createObjectURL(file);
-  
-
-       let pdfsToMerge = [url];
-
-       
-       const mergedPdf = await PDFDocument.create();
-       for (const pdfCopyDoc of pdfsToMerge) {
-         const pdfBytes = await fetch(pdfCopyDoc).then(res => res.arrayBuffer())
-
-         const pdf = await PDFDocument.load(pdfBytes);
-         const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-         copiedPages.forEach((page : any) => {
-           mergedPdf.addPage(page);
-         });
-       }
+    this.GET.GetBalanzaComprobacion(this.val.Get("txtFecha1").value, this.val.Get("txtFecha2").value, this.val.Get("cmbNivel").value, this.val.Get("cmbMoneda").value).subscribe(
+      {
+        next: (data) => {
 
 
-       const mergedPdfFile = await mergedPdf.save();
-       this.downloadFile(mergedPdfFile);
-  
-   
+          dialogRef.close();
+          let _json: any = data;
+
+          if (_json["esError"] == 1) {
+            if (this.cFunciones.DIALOG.getDialogById("error-servidor-msj") == undefined) {
+              this.cFunciones.DIALOG.open(DialogErrorComponent, {
+                id: "error-servidor-msj",
+                data: _json["msj"].Mensaje,
+              });
+            }
+          } else {
+
+            let datos: iDatos = _json["d"];
+            this.printPDFS(datos.d);
+
+
+
+          }
+
+        },
+        error: (err) => {
+
+
+          dialogRef.close();
+          document.getElementById("btnReporte-Balanza")?.removeAttribute("disabled");
+          if (this.cFunciones.DIALOG.getDialogById("error-servidor") == undefined) {
+            this.cFunciones.DIALOG.open(DialogErrorComponent, {
+              id: "error-servidor",
+              data: "<b class='error'>" + err.message + "</b>",
+            });
+          }
+
+        },
+        complete: () => {
+          document.getElementById("btnReporte-Balanza")?.removeAttribute("disabled");
+
+
+        }
+      }
+    );
+
+
+  }
+
+
+  async printPDFS(datos: any) {
+
+
+
+    let byteArray = new Uint8Array(atob(datos).split('').map(char => char.charCodeAt(0)));
+
+    var file = new Blob([byteArray], { type: 'application/pdf' });
+
+    let url = URL.createObjectURL(file);
+
+
+    let pdfsToMerge = [url];
+
+
+    let iframe = document.createElement('iframe');
+    iframe.id = "fFactura";
+    iframe.style.display = 'none';
+
+    iframe.src = url
+    document.body.appendChild(iframe);
+    iframe.onload = () => {
+      setTimeout(() => {
+        iframe.focus();
+       // iframe.contentWindow?.print();
+
+        let tabOrWindow : any = window.open(url, '_blank');
+        tabOrWindow.focus();
+
+
+      });
+    };
+
+
+    return;
+
+
+
+    if (this.cFunciones.MyBrowser() == "Firefox") {
+      let iframe = document.createElement('iframe');
+      iframe.id = "fFactura";
+      iframe.style.display = 'none';
+
+      iframe.src = url
+      document.body.appendChild(iframe);
+      iframe.onload = () => {
+        setTimeout(() => {
+          iframe.focus();
+         // iframe.contentWindow?.print();
+
+          let tabOrWindow : any = window.open(url, '_blank');
+          tabOrWindow.focus();
+
+
+        });
+      };
+
+    }
+    else {
+      const mergedPdf = await PDFDocument.create();
+      for (const pdfCopyDoc of pdfsToMerge) {
+        const pdfBytes = await fetch(pdfCopyDoc).then(res => res.arrayBuffer())
+
+        const pdf = await PDFDocument.load(pdfBytes);
+        const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+        copiedPages.forEach((page: any) => {
+          mergedPdf.addPage(page);
+        });
+      }
+
+
+      const mergedPdfFile = await mergedPdf.save();
+      this.downloadFile(mergedPdfFile);
     }
 
 
-    downloadFile(data: any) {
-      const blob = new Blob([data], { type: 'application/pdf' });
-      const url = window.URL.createObjectURL(blob);
-
-      printJS({
-        printable: url,
-        type: 'pdf',
-        onPrintDialogClose: undefined
-      })
-  
-    }
-  
 
 
-    ngDoCheck(): void {
-      ///CAMBIO DE FOCO
+  }
 
-      this.val.addFocus("txtFecha1", "txtFecha2", undefined);
-      this.val.addFocus("txtFecha2", "cmbNivel", undefined);
-      this.val.addFocus("cmbNivel", "cmbMoneda", undefined);
-      this.val.addFocus("cmbMoneda", "btnReporte-Balanza", "click");
-  
-      
-    }
-  
-    
+
+  downloadFile(data: any) {
+    const blob = new Blob([data], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(blob);
+
+    printJS({
+      printable: url,
+      type: 'pdf',
+      onPdfOpen: undefined,
+      onPrintDialogClose: undefined
+    })
+
+  }
+
+
+
+  ngDoCheck(): void {
+    ///CAMBIO DE FOCO
+
+    this.val.addFocus("txtFecha1", "txtFecha2", undefined);
+    this.val.addFocus("txtFecha2", "cmbNivel", undefined);
+    this.val.addFocus("cmbNivel", "cmbMoneda", undefined);
+    this.val.addFocus("cmbMoneda", "btnReporte-Balanza", "click");
+
+
+  }
+
+
 }
--- src/app/SHARED/class/cls_Funciones.ts
+++ src/app/SHARED/class/cls_Funciones.ts
@@ -160,6 +160,44 @@ export class Funciones {
    
   }
 
+
+  
+
+  public MyBrowser() {
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+      return 'Opera';
+    } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+      return 'Chrome';
+    } else if (navigator.userAgent.indexOf("Safari") != -1) {
+      return 'Safari';
+    } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+      return 'Firefox';
+    } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!(document as any).documentMode == true)) {
+      return 'IE';
+    } else {
+      return 'unknown';
+    }
+  }
+
+
+  getBrowserVersion() {
+    var userAgent = navigator.userAgent, tem,
+      matchTest = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if (/trident/i.test(matchTest[1])) {
+      tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+      return 'IE ' + (tem[1] || '');
+    }
+    if (matchTest[1] === 'Chrome') {
+      tem = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+      if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    matchTest = matchTest[2] ? [matchTest[1], matchTest[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem = userAgent.match(/version\/(\d+)/i)) != null) matchTest.splice(1, 1, tem[1]);
+    return matchTest.join(' ');
+  }
+
+  
+
 }
 
 
