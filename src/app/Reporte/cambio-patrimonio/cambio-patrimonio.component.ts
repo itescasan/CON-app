29f82d2197a738e8ce2712d1441910aefd652855
Douglas Martinez <doggymany@hotmail.com>
2025-12-04 10:09:59 -0600

    .

--- src/app/Reporte/cambio-patrimonio/cambio-patrimonio.component.ts
+++ src/app/Reporte/cambio-patrimonio/cambio-patrimonio.component.ts
@@ -5,6 +5,7 @@ import { Funciones } from 'src/app/SHARED/class/cls_Funciones';
 import { WaitComponent } from 'src/app/SHARED/componente/wait/wait.component';
 import { DialogErrorComponent } from 'src/app/SHARED/componente/dialog-error/dialog-error.component';
 import { iDatos } from 'src/app/SHARED/interface/i-Datos';
+import { getReporteFinanciero } from '../GET/get-Reporte-FinancierosCNT';
 
 
 @Component({
@@ -17,12 +18,96 @@ export class CambioPatrimonioComponent {
 
   public val = new Validacion();
 
-  V_Imprimir(): void {
+  constructor(private cFunciones: Funciones, private GET: getReporteFinanciero
+    ) {
+  
+      this.val.add("txtFecha", "1", "LEN>", "0", "Fecha", "Seleccione una fecha.");    
+      this.val.add("cmbMoneda", "1", "LEN>", "0", "Moneda", "Selecione una moneda");
+  
+  
+      this.val.Get("txtFecha").setValue(this.cFunciones.DateFormat(this.cFunciones.FechaServer, "yyyy-MM-dd"));    
+      this.val.Get("cmbMoneda").setValue(1);
+    }
 
+  V_Imprimir(): void {
  
     document.getElementById("btnReporte-CambioPatrimonio")?.setAttribute("disabled", "disabled");
-   
 
+    let dialogRef: any = this.cFunciones.DIALOG.getDialogById("wait");  
+  
+      if (dialogRef == undefined) {
+        dialogRef = this.cFunciones.DIALOG.open(
+          WaitComponent,
+          {
+            panelClass: "escasan-dialog-full-blur",
+            data: "",
+            id: "wait"
+          }
+        );
+  
+      }
+
+      this.GET.GetEstadoCambioPatromonio(this.cFunciones.DateFormat(this.val.Get("txtFecha").value, "yyyy-MM-dd"), this.cFunciones.DateFormat(this.val.Get("txtFecha").value, "yyyy-MM-dd"), this.val.Get("cmbMoneda").value).subscribe(
+        {
+          next: (data) => {  
+  
+            dialogRef.close();
+            let _json: any = data;
+  
+            if (_json["esError"] == 1) {
+              if (this.cFunciones.DIALOG.getDialogById("error-servidor-msj") == undefined) {
+                this.cFunciones.DIALOG.open(DialogErrorComponent, {
+                  id: "error-servidor-msj",
+                  data: _json["msj"].Mensaje,
+                });
+              }
+            } else {
+  
+              let datos: iDatos = _json["d"];
+              this.printPDFS(datos.d); 
+  
+            }
+  
+          },
+          error: (err) => {  
+  
+            dialogRef.close();
+            document.getElementById("btnReporte-CambioPatrimonio")?.removeAttribute("disabled");
+            if (this.cFunciones.DIALOG.getDialogById("error-servidor") == undefined) {
+              this.cFunciones.DIALOG.open(DialogErrorComponent, {
+                id: "error-servidor",
+                data: "<b class='error'>" + err.message + "</b>",
+              });
+            }
+  
+          },
+          complete: () => {
+            document.getElementById("btnReporte-CambioPatrimonio")?.removeAttribute("disabled");  
+  
+          }
+        }
+      );
     
   }
+
+    async printPDFS(datos: any) {  
+  
+      let byteArray = new Uint8Array(atob(datos).split('').map(char => char.charCodeAt(0)));
+  
+      var file = new Blob([byteArray], { type: 'application/pdf' });
+  
+      let url = URL.createObjectURL(file);
+  
+      let tabOrWindow : any = window.open(url, '_blank');
+      tabOrWindow.focus(); 
+  
+    }  
+  
+    ngDoCheck(): void {
+      ///CAMBIO DE FOCO  
+      
+      this.val.addFocus("txtFecha", "cmbMoneda", undefined);
+      this.val.addFocus("cmbMoneda", "btnReporte-CambioPatrimonio", "click");
+    }
+
 }
